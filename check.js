b8f8c13046d88cd63eee6201e3ecfffa46022b20
Marcus Silva <mvfsillva@gmail.com>
2018-08-25 02:55:24 -0300

    Fix rename file

---
+++ check.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const url = 'https://jsonblob.com/api/jsonblob/89eb069b-a7cf-11e8-bac7-e1d00cd5aa77';
+
+const colors = [
+  '#ffc300',
+  '#ffba04',
+  '#ffb108',
+  '#ffa80d',
+  '#ff9f11',
+  '#ff9615',
+  '#ff8d1a',
+  '#ff841e',
+  '#ff7b22',
+  '#ff7226',
+  '#ff692b',
+  '#ff602f',
+  '#ff5733',
+];
+
+const template = (id, word, colorhex) => (
+  `<div style="display: inline;" id="${id}">
+    <mark style="background-color: ${colorhex}; color: black;">
+      ${word}
+    </mark>
+  </div>`
+);
+
+fetch(url).then((response) => { 
+  const data = response.json()
+  data.then(res => {
+    res.map(({ word, id, relevance }) => {
+      const selected = (relevance >= 0 && relevance <= 1) ? (relevance * 10) : 0;
+      const colorhex = colors[selected];
+      document.body.innerHTML = document.body.innerHTML.split(word).join(template(id, word, colorhex));
+    });
+
+    chrome.runtime.sendMessage(`Foram encontradas ${res.length} correspondências`);
+  });
+}).catch(err => console.error(err));
