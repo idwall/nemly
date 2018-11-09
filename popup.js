aef44334eebae0c83d3ebb8a402ddbd9f96c80cb
mvfsillva <mvfsillva@gmail.com>
2018-11-09 16:25:09 -0200

    add anonymous function

--- popup.js
+++ popup.js
@@ -1,7 +1,17 @@
 'use strict';
 
-search.onclick = () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    chrome.tabs.executeScript({ file: 'check.js' });
-  });
-};
+(() => {
+  const _search = document.querySelector('[data-js="search"]')
+  const _result = document.querySelector('[data-js="result"]')
+  _result.style.display = 'none';
+  
+  search.onclick = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      chrome.tabs.executeScript({ file: 'check.js' });
+    });
+
+    _search.parentElement.removeChild(_search);
+    _result.style.display = 'block';
+  };
+
+})()
