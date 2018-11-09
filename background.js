48a6b0fa230452a72f33ee3bdf922bdd3ef18134
Bu Kinoshita <bukinoshita@gmail.com>
2018-11-09 13:37:03 -0200

    fix `_` not defined

--- background.js
+++ background.js
@@ -1,28 +1,30 @@
-'use strict';
+'use strict'
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.declarativeContent.onPageChanged.removeRules(_, () => {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [
-        new chrome.declarativeContent.PageStateMatcher({
-          // Aqui podem ser adicionadas condições para que a extenssão seja ativada
-        })
-      ],
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            // Aqui podem ser adicionadas condições para que a extenssão seja ativada
+          })
+        ],
         actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
+      }
+    ])
+  })
 
-    chrome.runtime.onConnect.addListener(port => {
-      port.onMessage.addListener(msg => {
-        port.postMessage({ counter: msg.counter + 1 });
-      });
-    });
+  chrome.runtime.onConnect.addListener(port => {
+    port.onMessage.addListener(msg => {
+      port.postMessage({ counter: msg.counter + 1 })
+    })
+  })
 
-    chrome.extension.onRequest.addListener((request, sender, sendResponse) => {
-      zsendResponse({ counter: request.counter + 1 });
-    });
+  chrome.extension.onRequest.addListener((request, sender, sendResponse) => {
+    zsendResponse({ counter: request.counter + 1 })
+  })
 
-    chrome.runtime.onMessage.addListener((response, sender, sendResponse) => {
-      alert(response);
-    });
-});
+  chrome.runtime.onMessage.addListener((response, sender, sendResponse) => {
+    alert(response)
+  })
+})
